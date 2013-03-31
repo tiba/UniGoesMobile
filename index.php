3c1489d946f5a0638a97c64561d826205aa4287c
tiba <tinabeier@web.de>
2013-03-31 15:43:21 +0200

    index.php für suche gebugfixed

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         $url = $_SERVER['REQUEST_URI'];//$_REQUEST['path'];
         $url = str_replace('/UniGoesMobile','',$url);
         //abfrage für Suche: die läuft über eine andere Subdomain...
-        if(strpos("cgi-bin/searchext.cgi",$url)!== false){
+        if(strpos($url,"cgi-bin/searchext.cgi")!== false){
         	$data = file_get_contents('http://websearch.uni-passau.de/' . $url);
         }else{
         	$data = file_get_contents('http://uni-passau.de/' . $url);
